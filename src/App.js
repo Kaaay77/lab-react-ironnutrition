e259079c9a624791cd3631c08e8e270b7301e74f
Kaaay77 <96114448+Kaaay77@users.noreply.github.com>
2022-07-13 19:10:34 +0200

    iteracion 5 terminada

--- src/App.js
+++ src/App.js
@@ -5,27 +5,44 @@ import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import { useState } from 'react';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
 const [ food, setFood ] = useState(foods)
-
+const [input, setInput] = useState('')
+const [comidaFiltrada, setComidaFiltrada] = useState(food)
 const addNewFood = (newFood) =>{
   const updateFood = [...food, newFood];
   setFood(updateFood);
 }
+
+const searchFood = (e) => {
+  console.log(e,e.target.value)
+  setInput(e.target.value) 
+  const valueInput = e.target.value.toLocaleLowerCase()
+  const filteredFood = food.filter((comida)=>{
+   const incluyeComida = comida.name.toLocaleLowerCase()
+   return incluyeComida.includes(valueInput)
+  })
+  setComidaFiltrada(filteredFood)
+ }
 
   return <div className="App App-Contain">
 <h1 style={{color:'#fff'}} >FOOD LIST 🍔🥑🌮</h1>
 
      <div className='form'>
   <AddFoodForm addFood={addNewFood}/>
-     </div>     
+     </div>  
+
+     <div>
+       <Search input={input} searchFood={searchFood}/>
+     </div>   
 
       <div className='foodBox'>
           
         {
-
-          food.map((comida) => {
+          comidaFiltrada.map((comida) => {
+            
             return(
 
                 <div>
@@ -36,9 +53,11 @@ const addNewFood = (newFood) =>{
                     calories={comida.calories}
                     servings={comida.servings}
                   />
+
               </div>
             )
           }) 
+
         }
       </div>
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,16 @@
 import { Divider, Input } from 'antd';
 
 // Iteration 5
-function Search(props) {
+function Search({input, searchFood}) {
+
+    
     
   return (
     <>
       <Divider>Search</Divider>
 
       <label>Search</label>
-      <Input value={undefined} type="text" onChange={() => {}} />
+      <Input value={input} type="text" onChange={(e) => searchFood(e)} />
     </>
   );
 }
